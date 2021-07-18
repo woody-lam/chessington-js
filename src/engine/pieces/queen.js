b9359c159ab3aad0b0458f692f3cce6862fddbc7
Woody Lam <woody.lam@softwire.com>
2021-07-19 00:01:36 +0100

    Pass Red-5

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,11 +1,8 @@
-import Piece from './piece';
+import MultiMovePiece from './MultiMovePiece';
+import Square from '../square'
 
-export default class Queen extends Piece {
-    constructor(player) {
-        super(player);
-    }
-
-    getAvailableMoves(board) {
-        return new Array(0);
+export default class Queen extends MultiMovePiece {
+    constructor(player, steps = [new Square(0,1), new Square(1,0), new Square(1,1), new Square(1,-1)]) {
+        super(player, steps);
     }
 }
