4c6e5a85a33d70a8408c1ce926851e5c57f77850
Woody Lam <woody.lam@softwire.com>
2021-07-19 00:08:39 +0100

    Pass Red-7

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,11 +1,8 @@
-import Piece from './piece';
+import SingleMovePiece from './singlemovepiece';
+import Square from '../square';
 
-export default class King extends Piece {
-    constructor(player) {
-        super(player);
-    }
-
-    getAvailableMoves(board) {
-        return new Array(0);
+export default class Knight extends SingleMovePiece {
+    constructor(player, steps = [new Square(1,0), new Square(1,1), new Square(0,1), new Square(1,-1)]) {
+        super(player, steps);
     }
 }
